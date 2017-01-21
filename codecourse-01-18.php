ea33090e385b4769946e1c34cb2c8d44d77750bd
ML Managuelod <mlmanaguelod@gmail.com>
2017-01-21 12:00:16 +0800

    initial commit

---
+++ codecourse-01-18.php
@@ -0,0 +1,66 @@
+<?php
+
+
+
+
+
+// echo "Today is " . date("Y/m/d") . "<br>";
+// echo "Today is " . date("Y.m.d") . "<br>";
+// echo "Today is " . date("Y-m-d") . "<br>";
+// echo "Today is " . date("l");
+
+
+
+
+// $cars = array(
+//     array('Volvo', 22, 18),
+//     array('BMW', 15, 13),
+//     array('Saab', 5, 2),
+//     array('Land Rover', 27, 15)
+// );
+
+
+// echo '<pre>' , var_dump($cars) , '</pre>';
+// echo $cars[0][0].": In stock: ".$cars[0][1].", sold: ".$cars[0][2].".<br>";
+// echo $cars[1][0].": In stock: ".$cars[1][1].", sold: ".$cars[1][2].".<br>";
+// echo $cars[2][0].": In stock: ".$cars[2][1].", sold: ".$cars[2][2].".<br>";
+// echo $cars[3][0].": In stock: ".$cars[3][1].", sold: ".$cars[3][2].".<br>";
+//Dipping into arrays in php, @codecourse tutorials.
+
+// $names = [ml => 25, rona => 20, dianne => 22];
+
+// When choose to echo certain array.
+//echo $names[1];
+
+// $users = [
+//     ['username' => 'ml'], ['username' => 'mlm']
+
+// ];
+
+// echo $users[0]['username']; //This is multi-dimensional array.
+
+// $users = [
+//     [
+//         'username' => 'mlemz',
+//         'email' => 'mlmanaguelod@gmail.com',
+//         'likes' => ['designing', 'programmming']
+//     ],
+//     [
+//         'username' => 'ml',
+//         'email' => 'mlm.managuelod@gmail.com',
+//         'likes' => ['dancing', 'singing']
+//     ]
+
+// ];
+
+// foreach ($users as $user){
+//     // var_dump($user);
+//     echo $user['username'] . '<br>';
+
+    // echo '<pre>', var_dump($users) ,'</pre>';}
+
+
+
+
+
+?>
